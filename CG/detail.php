98752ac891147ffa6d2eaf47f4edaeb06e5c5ca7
NgeZY <zheyunge@gmail.com>
2024-09-10 09:07:38 +0800

    Remove cart icon right top

--- CG/detail.php
+++ CG/detail.php
@@ -56,13 +56,6 @@
                     </div>
                 </form>
             </div>
-            <div class="col-lg-3 col-6 text-right">
-               
-                <a href="" class="btn border">
-                    <i class="fas fa-shopping-cart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-            </div>
         </div>
     </div>
     <!-- Topbar End -->
