f04116a65a9b70625508f8ff8143b8b492b3de7e
NgeZY <zheyunge@gmail.com>
2024-11-03 23:45:46 +0800

    Update Viewhistoryorder.php

--- CG/Viewhistoryorder.php
+++ CG/Viewhistoryorder.php
@@ -114,11 +114,11 @@ ob_end_flush();
     <!-- Page Header Start -->
     <div class="container-fluid bg-secondary mb-5">
         <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
-            <h1 class="font-weight-semi-bold text-uppercase mb-3">Shopping Cart</h1>
+            <h1 class="font-weight-semi-bold text-uppercase mb-3">Order History</h1>
             <div class="d-inline-flex">
                 <p class="m-0"><a href="../index.php">Home</a></p>
                 <p class="m-0 px-2">-</p>
-                <p class="m-0">Shopping Cart</p>
+                <p class="m-0">Order History</p>
             </div>
         </div>
     </div>
